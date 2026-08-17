476fe0820b514783da56a6e828376d8bf10099e5
Diyan Raykov <diyanrayikov@gmail.com>
2026-08-17 13:56:51 +0300

    Added comments

--- Challenges/Day_2/solution/solution.explorer.sh
+++ Challenges/Day_2/solution/solution.explorer.sh
@@ -1,9 +1,13 @@
 echo "Welcome to the Interactive File and Directory Explorer!"
-
+#initial variable value needed so you can go into loop
 input="sample"
+#until the variabla is not empty do this
 until [[ -z "$input" ]]
 do
+	#list directory
 	ls -lah
+	#count characters in string
         echo -n "$input" | wc -m
+	#read string
         read -p "Enter a line of text (Press Enter without text to exit):" input
 done;
