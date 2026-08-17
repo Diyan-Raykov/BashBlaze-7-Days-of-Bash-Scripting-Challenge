6176be6f205aa3df33698ecb83464d396d60994e
Diyan Raykov <diyanrayikov@gmail.com>
2026-08-17 13:52:30 +0300

    V3

--- Challenges/Day_2/solution/solution.explorer.sh
+++ Challenges/Day_2/solution/solution.explorer.sh
@@ -1,11 +1,9 @@
 echo "Welcome to the Interactive File and Directory Explorer!"
 
 input="sample"
-#read -p "Enter a line of text (Press Enter without text to exit): Hello, this is a sample line." input
 until [[ -z "$input" ]]
 do
-	file *
-        #ls -lah
+	ls -lah
         echo -n "$input" | wc -m
-        read -p "Enter a line of text (Press Enter without text to exit): Hello, this is a sample line." input
+        read -p "Enter a line of text (Press Enter without text to exit):" input
 done;
