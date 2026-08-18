d200dd359b11fab7ed44be7ad4b90f50b4bfec1f
Diyan-Raykov <80852853+Diyan-Raykov@users.noreply.github.com>
2026-08-18 22:47:18 +0300

    Update  comments solution.backup.sh
    Updated comments

--- Challenges/Day_2/solution/solution.backup.sh
+++ Challenges/Day_2/solution/solution.backup.sh
@@ -4,11 +4,10 @@
 read -p "enter directory you want to use as backup storage " DirInput
 
 #backup location
-
+#2>/dev/null to evade ..Day_2/solution: Is a directory message
 backup_location=$($DirInput)backup_$(date +%Y-%m-%d_%H-%M-%S) 2>/dev/null
 
 #make backup directory
-
 mkdir -p $backup_location
 
 #tell user that the dir is created
@@ -18,8 +17,9 @@ echo -e "Backup created: $backup_location"
 cp $(find . -maxdepth 1 -type f) $backup_location
 #delete old files
 count=$(ls -ltrd $DirInput/backup* |wc -l)
-
+#check if the dirs are more than 3
 math=$(( $count-3 ))
+#check if variable is greater than 0 to avoid error message
 if [ $math -gt 0 ]; then
 ls -ltrd */ | head -$math |xargs rm -fr
 fi
